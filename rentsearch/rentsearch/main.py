41921dd21c778e286ac3953374487b1b5e8ad908
Shane Nolan <shanemichealnolan@gmail.com>
2021-10-18 17:49:05 +0100

    Next page functionality.

--- rentsearch/rentsearch/main.py
+++ rentsearch/rentsearch/main.py
@@ -7,15 +7,26 @@ import requests
 from bs4 import BeautifulSoup
 
 def lambda_handler(event, context):
+    PROPERTIES_PER_PAGE = 20
+    NO_PROPERTIES_FOUND = "We didn't find any properties"
+
     base_url = 'https://www.daft.ie'
 
     sqs = boto3.resource('sqs')
     queue = sqs.get_queue_by_name(QueueName=os.environ['sqsname'])
 
-    resp = requests.get(f'{base_url}/property-for-rent/limerick-city?sort=publishDateDesc')
+    counter = 0
+    while True:
+        resp = requests.get(f'{base_url}/property-for-rent/limerick?sort=publishDateDesc&pageSize=20&from={counter}')
+        html = resp.text
 
-    soup = BeautifulSoup(resp.text, 'html.parser')
-    for link in soup.select('a[href^="/for-rent/"]'):
-        queue.send_message(MessageBody=f'{base_url}{link["href"]}')
+        if NO_PROPERTIES_FOUND in html:
+            break
+
+        soup = BeautifulSoup(html, 'html.parser')
+        for link in soup.select('a[href^="/for-rent/"]'):
+            queue.send_message(MessageBody=f'{base_url}{link["href"]}')
+        
+        counter += PROPERTIES_PER_PAGE
 
     return {'statusCode': 200}
